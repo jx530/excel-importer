510c9eeb2dabbfddc8c2a259a1e325a500b9d80b
holmofy <hff1996723@163.com>
2023-07-19 10:50:55 +0800

    commit

--- src/main/java/com/jx530/excelimporter/model/BaseModel.java
+++ src/main/java/com/jx530/excelimporter/model/BaseModel.java
@@ -1,6 +1,8 @@
 package com.jx530.excelimporter.model;
 
 import com.alibaba.excel.annotation.ExcelIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Version;
@@ -15,19 +17,28 @@ import java.time.LocalDateTime;
 public class BaseModel {
 
     @Id
+    @JsonIgnore
     @ExcelIgnore
-    private String id;
+    private Long id;
 
     @Version
+    @JsonIgnore
     @ExcelIgnore
-    private Integer version;
+    private Integer __version;
 
     @CreatedDate
+    @JsonIgnore
     @ExcelIgnore
     private LocalDateTime __created;
 
     @LastModifiedDate
+    @JsonIgnore
     @ExcelIgnore
     private LocalDateTime __modified;
+
+    @JsonProperty("id")
+    public String getIdString() {
+        return Long.toString(id);
+    }
 
 }
